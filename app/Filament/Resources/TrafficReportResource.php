e6b58724c662aaee7b41e0794abd8a3e16114785
fremasadi <fremasadii@gmail.com>
2025-06-09 12:53:57 +0700

    feat

--- app/Filament/Resources/TrafficReportResource.php
+++ app/Filament/Resources/TrafficReportResource.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
 
 class TrafficReportResource extends Resource
 {

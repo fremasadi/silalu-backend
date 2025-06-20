80f46336bb82a4aadb6426026ee7e9ecc642ab01
fremasadi <fremasadii@gmail.com>
2025-06-21 02:50:22 +0700

    feat

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -1,5 +1,6 @@
 <?php
 
+// app/Filament/Pages/Dashboard.php
 namespace App\Filament\Pages;
 
 use Filament\Pages\Dashboard as BaseDashboard;
@@ -12,14 +13,22 @@ class Dashboard extends BaseDashboard
     
     protected static ?string $navigationIcon = 'heroicon-o-home';
     
+    protected static ?int $navigationSort = 1;
+    
     public function getWidgets(): array
     {
         return [
             \App\Filament\Widgets\TrafficStatsOverview::class,
+            \App\Filament\Widgets\DailyReportsOverview::class,
             \App\Filament\Widgets\TrafficReportChart::class,
             \App\Filament\Widgets\TrafficReportStatusChart::class,
             \App\Filament\Widgets\RecentTrafficReports::class,
             \App\Filament\Widgets\TopTrafficLocations::class,
         ];
+    }
+    
+    public function getColumns(): int | string | array
+    {
+        return 2;
     }
 }

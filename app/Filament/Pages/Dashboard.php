ab320272b11fa0b8e8877abf5e9dd8eb40f0a55a
fremasadi <fremasadii@gmail.com>
2025-06-21 02:55:17 +0700

    feat

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -21,7 +21,7 @@ class Dashboard extends BaseDashboard
             \App\Filament\Widgets\TrafficStatsOverview::class,
             \App\Filament\Widgets\DailyReportsOverview::class,
             \App\Filament\Widgets\TrafficReportChart::class,
-            \App\Filament\Widgets\TrafficReportStatusChart::class,
+            // \App\Filament\Widgets\TrafficReportStatusChart::class,
             \App\Filament\Widgets\RecentTrafficReports::class,
             // \App\Filament\Widgets\TopTrafficLocations::class,
         ];

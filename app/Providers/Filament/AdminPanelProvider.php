ceac016669dd6acb744f9de41165b7f37fdbb504
fremasadi <fremasadii@gmail.com>
2025-06-23 23:09:58 +0700

    feat

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,8 +54,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-                \App\Http\Middleware\EnsureUserIsAdmin::class, // Tambahkan ini
-
             ]);
     }
 }

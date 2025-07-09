2047ecb017a0f561659dc3d21c5bdca1ea736489
fremasadi <fremasadii@gmail.com>
2025-07-09 13:50:00 +0700

    feat

---
+++ app/Filament/Widgets/TrafficStatsOverview.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use App\Models\TrafficReport;
+use Illuminate\Support\Facades\DB;
+
+class TrafficStatsOverview extends ChartWidget
+{
+    
+}

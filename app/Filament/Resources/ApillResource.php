2f00f38468550db87422fbfa66787ae6325bbe89
fremasadi <fremasadii@gmail.com>
2025-07-10 19:34:57 +0700

    feat

--- app/Filament/Resources/ApillResource.php
+++ app/Filament/Resources/ApillResource.php
@@ -3,29 +3,27 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ApillResource\Pages;
+use App\Filament\Resources\ApillResource\RelationManagers;
 use App\Models\Apill;
 use Filament\Forms;
-use Filament\Resources\Form;
+use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ApillResource extends Resource
 {
     protected static ?string $model = Apill::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-traffic-light';
-
-    protected static ?string $navigationGroup = 'Manajemen Lalu Lintas';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->label('Nama APILL')
-                    ->required()
-                    ->maxLength(255),
+                //
             ]);
     }
 
@@ -33,24 +31,26 @@ class ApillResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('id')->sortable(),
-                Tables\Columns\TextColumn::make('name')
-                    ->label('Nama APILL')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->label('Dibuat')
-                    ->dateTime('d M Y - H:i'),
+                //
             ])
-            ->defaultSort('id', 'desc')
-            ->filters([])
+            ->filters([
+                //
+            ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
             ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
     }
 
     public static function getPages(): array
